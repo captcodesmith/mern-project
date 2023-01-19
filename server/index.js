273226de51eed7a5094cec66ea987a9eb7c61dd5
captcodesmith <naman.personal.sharma@gmail.com>
2023-01-19 21:22:55 +0530

    feat: dummy data added intot the database

--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ import postRoutes from './routes/posts.js';
 import { register } from './controllers/auth.js';
 import { createPost } from './controllers/posts.js';
 import { verifyToken } from './middleware/auth.js';
+import User from './models/User.js';
+import Post from './models/posts.js';
+import { users, posts } from './data/index.js';
 
 /* Configurations */
 const __filename = fileURLToPath(import.meta.url); // location of the index.js file
@@ -67,5 +70,9 @@ mongoose
   })
   .then((value) => {
     app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
+
+    /* Add data one time */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
